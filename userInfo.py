b316288830ca22b686a6ba42c462871cc6b37a54
Brandova <150844341+Brandova@users.noreply.github.com>
2024-09-28 14:57:18 -0700

    Update userInfo.py

--- userInfo.py
+++ userInfo.py
@@ -1,6 +1,6 @@
 class UserData:
-    def __init__(self, income, gender, race,
-                  major, university, location):
+    def __init__(self, income: float, gender: str, race: str,
+                 major: str, university: str, location: str):
         self.__income = income
         self.__gender = gender
         self.__race = race
@@ -8,41 +8,40 @@ class UserData:
         self.__university = university
         self.__location = location
 
-    def get_income(self):
+    def get_income(self) -> float:
         return self.__income
 
-    def get_gender(self):
+    def get_gender(self) -> str:
         return self.__gender
 
-    def get_race(self):
+    def get_race(self) -> str:
         return self.__race
 
-    def get_major(self):
+    def get_major(self) -> str:
         return self.__major
 
-    def get_university(self):
+    def get_university(self) -> str:
         return self.__university
 
-    def get_location(self):
+    def get_location(self) -> str:
         return self.__location
     
-    def set_income(self, income):
+    def set_income(self, income: float) -> None:
+        if income < 0:
+            raise ValueError("Income must be a non-negative value.")
         self.__income = income
 
-    def set_gender(self, gender):
+    def set_gender(self, gender: str) -> None:
         self.__gender = gender
 
-    def set_race(self, race):
+    def set_race(self, race: str) -> None:
         self.__race = race
 
-    def set_major(self, major):
+    def set_major(self, major: str) -> None:
         self.__major = major
 
-    def set_university(self, university):
+    def set_university(self, university: str) -> None:
         self.__university = university
 
-    def set_location(self, location):
+    def set_location(self, location: str) -> None:
         self.__location = location
-
-
-        
