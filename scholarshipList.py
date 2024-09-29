a2d9a1924cfea33907b324a65a03da919d63450a
Brandova <150844341+Brandova@users.noreply.github.com>
2024-09-28 22:36:27 -0700

    Update scholarshipList.py

--- scholarshipList.py
+++ scholarshipList.py
@@ -1,13 +1,13 @@
-from scholarship import Scholarship
+from augmentedScholarship import AugmentedScholarship
 
 class ScholarshipList:
     def __init__(self):
         self.__data = set()
 
-    def add_scholarship(self, new_scholarship: Scholarship) -> None:
+    def add_scholarship(self, new_scholarship: AugmentedScholarship) -> None:
         self.__data.add(new_scholarship)
 
-    def remove_scholarship(self, target: Scholarship) -> bool:
+    def remove_scholarship(self, target: AugmentedScholarship) -> bool:
         if target in self.__data:
             self.__data.remove(target)
             return True
