f6ed36a8798e61203f58100a396cc1f19e7203be
Brandova <150844341+Brandova@users.noreply.github.com>
2024-09-28 20:47:49 -0700

    Update scholarship.py

--- scholarship.py
+++ scholarship.py
@@ -1,10 +1,10 @@
 from datetime import date
 
 class Scholarship:
-    def __init__(self, favorited: bool, name: str, gender: str, merit_based: bool,
+    def __init__(self, id: int, name: str, gender: str, merit_based: bool,
                  ethnicity: str, university: str, location: str, reward: float, 
-                 LGBT: bool, extras: str, due_date: date):
-        self.__favorited = favorited
+                 LGBT: bool, extras: str, due_date: date, essay_required: bool, description: str):
+        self.__id = id
         self.__name = name
         self.__gender = gender
         self.__merit_based = merit_based
@@ -15,18 +15,20 @@ class Scholarship:
         self.__LGBT = LGBT
         self.__extras = extras
         self.__due_date = due_date
+        self.__essay_required = essay_required
+        self.__description = description
 
     def __hash__(self) -> int:
-        return hash((self.__favorited, self.__name, self.__merit_based, 
+        return hash((self.__id, self.__name, self.__merit_based, 
                      self.__gender, self.__ethnicity, self.__university, 
                      self.__location, self.__reward, self.__LGBT, 
-                     self.__extras, self.__due_date))
+                     self.__extras, self.__due_date, self.__essay_required, self.__description))
 
     def __eq__(self, other: 'Scholarship') -> bool:
         if not isinstance(other, Scholarship):
             return NotImplemented
         return (
-            self.__favorited == other.__favorited and
+            self.__id == other.__id and
             self.__name == other.__name and
             self.__merit_based == other.__merit_based and
             self.__gender == other.__gender and
@@ -36,13 +38,14 @@ class Scholarship:
             self.__reward == other.__reward and
             self.__LGBT == other.__LGBT and
             self.__extras == other.__extras and
-            self.__due_date == other.__due_date 
+            self.__due_date == other.__due_date and
+            self.__essay_required == other.__essay_required and
+            self.__description == other.__description
         )
 
-    # Getters
-    def get_favorited(self) -> bool:
-        return self.__favorited
-    
+    def get_id(self) -> int:
+        return self.__id
+
     def get_name(self) -> str:
         return self.__name
 
@@ -73,9 +76,14 @@ class Scholarship:
     def get_due_date(self) -> date:
         return self.__due_date
     
-    # Setters
-    def set_favorited(self, favorited: bool) -> None:
-        self.__favorited = favorited
+    def get_essay_required(self) -> bool:
+        return self.__essay_required
+    
+    def get_description(self) -> str:
+        return self.__description
+    
+    def set_id(self, id: int) -> None:
+        self.__id = id
 
     def set_name(self, name: str) -> None:
         self.__name = name
@@ -106,3 +114,9 @@ class Scholarship:
 
     def set_due_date(self, due_date: date) -> None:
         self.__due_date = due_date
+
+    def set_essay_required(self, essay_required: bool) -> None:
+        self.__essay_required = essay_required
+    
+    def set_description(self, description: str) -> None:
+        self.__description = description
