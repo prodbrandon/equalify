d93f3c11dc7cf3998b1864935afcd14383906bcc
prodbrandon <132955239+prodbrandon@users.noreply.github.com>
2024-10-04 20:31:15 -0700

    Update Home.py -- We Won!

--- Home.py
+++ Home.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title('''🎓 :rainbow[Equalify Home]''')
 st.write("Empowering underrepresented communities with scholarships for students!")
+st.code("Winner of Sunhacks - MLH - Best DEI Hack sponsored by Fidelity")
+st.link_button("Check it out on DevPost!", "https://devpost.com/software/equalify-xgqy7w")
+
+st.balloons()
